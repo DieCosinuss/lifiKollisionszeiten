25c86d59f36f5bd724432a08a1796ec891017921
Andreas Klasen <klasen@hhbk.de>
2023-12-03 22:26:36 +0100

    Erstellen der Beispiel-XML-Datei des aktuellen Autos

--- lifiCollisionTime/Program.cs
+++ lifiCollisionTime/Program.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace lifiCollisionTime
 {
     internal class Program
     {
+        static void createMyCar()
+        {
+            // Create data of myself in XML.
+            XDocument aktuellerVerkehrsteilnehmer = new XDocument(
+                new XElement("verkehrsteilnehmende",
+                    new XElement("auto",
+                        new XElement("geschwindigkeit", "50"),
+                        new XElement("beschleunigung", "20")
+                    )
+                )
+            );
+            aktuellerVerkehrsteilnehmer.Save("lifiMeinDatenpaket.xml");
+        }
+
         static void Main(string[] args)
         {
-
+            // create xml-file for data of the current car
+            createMyCar();
         }
     }
 }
